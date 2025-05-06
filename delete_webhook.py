edec5b7453c01570fedeb67a467a4a8426a86b9e
sanit888 <sanit7889@gmail.com>
2025-05-06 09:02:14 +0300

    Update delete_webhook.py

--- delete_webhook.py
+++ delete_webhook.py
@@ -3,7 +3,7 @@ import asyncio
 import os
 
 async def delete_webhook():
-    bot = Bot(token=os.getenv("BOT_TOKEN"))
+    bot = Bot(token=os.getenv("8085072986:AAGkWbFj_JKl9mrZwG99ixbF6M4R3Zsayw8"))
     await bot.delete_webhook(drop_pending_updates=True)
     print("✅ Webhook удалён")
 
